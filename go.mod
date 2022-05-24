601e468689fb3f109029ab7b4076d01a3e080f88
sano11o1 <mashio.tech@gmail.com>
2022-05-24 12:57:39 +0900

    send sms

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/sano11o1/twilio-sample
 
 go 1.18
+
+require (
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/twilio/twilio-go v0.26.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/twilio/twilio-go"
+	openapi "github.com/twilio/twilio-go/rest/api/v2010"
 )
 
 func main() {
-	fmt.Println("hello")
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		fmt.Printf("読み込み出来ませんでした: %v", err)
+	}
+	from := os.Getenv("TWILIO_FROM_PHONE_NUMBER")
+	to := os.Getenv("TWILIO_TO_PHONE_NUMBER")
+
+	accountSid := os.Getenv("TWILIO_ACCOUNT_SID")
+	authToken := os.Getenv("TWILIO_AUTH_TOKEN")
+	client := twilio.NewRestClientWithParams(twilio.ClientParams{
+		Username: accountSid,
+		Password: authToken,
+	})
+
+	params := &openapi.CreateMessageParams{}
+	params.SetTo(to)
+	params.SetFrom(from)
+	params.SetBody("Hello Friend")
+
+	resp, err := client.Api.CreateMessage(params)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		response, _ := json.Marshal(*resp)
+		fmt.Println("Response: " + string(response))
+	}
 }
