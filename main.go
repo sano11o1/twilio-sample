a0c4601ee17fe014c08647518a9e23c584a42013
sano11o1 <mashio.tech@gmail.com>
2022-05-24 15:56:11 +0900

    SMS認証のサンプルコード

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/twilio/twilio-go"
-	openapi "github.com/twilio/twilio-go/rest/api/v2010"
+	openapi "github.com/twilio/twilio-go/rest/verify/v2"
 )
 
 func main() {
@@ -16,26 +16,51 @@ func main() {
 	if err != nil {
 		fmt.Printf("読み込み出来ませんでした: %v", err)
 	}
-	from := os.Getenv("TWILIO_FROM_PHONE_NUMBER")
+	// from := os.Getenv("TWILIO_FROM_PHONE_NUMBER")
 	to := os.Getenv("TWILIO_TO_PHONE_NUMBER")
 
 	accountSid := os.Getenv("TWILIO_ACCOUNT_SID")
 	authToken := os.Getenv("TWILIO_AUTH_TOKEN")
+	serviceSid := os.Getenv("VERIFY_SERVICE_SID")
 	client := twilio.NewRestClientWithParams(twilio.ClientParams{
 		Username: accountSid,
 		Password: authToken,
 	})
 
-	params := &openapi.CreateMessageParams{}
+	params := &openapi.CreateVerificationParams{}
 	params.SetTo(to)
-	params.SetFrom(from)
-	params.SetBody("Hello Friend")
+	params.SetChannel("sms")
 
-	resp, err := client.Api.CreateMessage(params)
+	resp, err := client.VerifyV2.CreateVerification(serviceSid, params)
+
 	if err != nil {
 		fmt.Println(err.Error())
 	} else {
 		response, _ := json.Marshal(*resp)
 		fmt.Println("Response: " + string(response))
+	}
+
+	var inputCode string
+
+	fmt.Print("Enter Code > ")
+
+	inputCodeError, err := fmt.Scanln(&inputCode)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(inputCodeError)
+	}
+
+	checkParams := &openapi.CreateVerificationCheckParams{}
+	checkParams.SetTo(to)
+	checkParams.SetCode(inputCode)
+
+	checkResp, err := client.VerifyV2.CreateVerificationCheck(serviceSid, checkParams)
+	if err != nil {
+		fmt.Println(err)
+	} else if *checkResp.Status == "approved" {
+		fmt.Println("認証成功")
+	} else {
+		fmt.Println("認証失敗")
 	}
 }
